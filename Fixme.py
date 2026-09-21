15cbe30081f1d1c13657bca3e450133ee0557df0
David Aguinada <david.aguinada@gmail.com>
2026-09-20 20:24:43 -0700

    Fix evens function

--- Fixme.py
+++ Fixme.py
@@ -4,3 +4,10 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
+    output  = []
+
+    for num in range(n + 1):
+        if num % 2 == 0:
+            output += [num]
+
+    return output
