57bf7f0573aa07614f8f6565c7a2e604920d8db2
David Aguinada <david.aguinada@gmail.com>
2026-09-20 21:11:41 -0700

    Fix spacing

--- Fixme.py
+++ Fixme.py
@@ -4,7 +4,7 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    output  = []
+    output = []
 
     for num in range(n + 1):
         if num % 2 == 0:
